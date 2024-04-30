ce67c454abdcb59b5e5954eb9756f1cd2f727f83
Nguyễn Bình Minh <nbinhminh158@gmail.com>
2024-04-30 18:03:39 +0700

    feat: filter by status in seller history

--- src/pages/history/seller/index.vue
+++ src/pages/history/seller/index.vue
@@ -1,8 +1,8 @@
 <template>
     <div v-loading="loading" class="w-full">
-        <div class="w-full mt-6 ml-2">
+        <div class="mt-6 ml-2">
             <div class="flex justify-between gap-5 my-[42px]">
-                <span class="font-bold text-2xl flex items-center justify-center">Sold</span>
+                <span class="font-bold text-2xl flex items-center justify-center">Seller history</span>
                 <div class="w-1/2">
                     <div class="w-full flex items-center justify-center">
                         <el-input
@@ -22,8 +22,8 @@
                 </div>
             </div>
 
-            <div class="flex items-center gap-4 pb-[10px] my-4">
-                <div v-for="item in listProductStatus" :key="item.value" class="">
+            <div class="flex items-center gap-4 pb-[10px] my-4 justify-between">
+                <!-- <div v-for="item in listProductStatus" :key="item.value" class="">
                     <span
                         :class="{ 'bg-[#ededed]': selectedItem === item.value }"
                         class="px-4 py-1 flex items-center justify-center border-gray-600 border-[1px] rounded-full bg-[#f7f7f7] hover:cursor-pointer hover:bg-[#ededed]"
@@ -31,15 +31,30 @@
                     >
                         {{ item.text }}
                     </span>
-                </div>
+                </div> -->
+                <el-select
+                    v-model="status"
+                    placeholder="Filter by status"
+                    size="large"
+                    style="width: 240px"
+                    @change="filterStatus"
+                >
+                <el-option
+                    v-for="item in listProductStatus"
+                    :key="item.value"
+                    :label="item.text"
+                    :value="item.value"
+                />
+                </el-select>
+
+                <el-button color="#626aef" style="font-size: medium" round size="large" @click="createAuction"
+                        >Create Auction</el-button
+                    >
             </div>
 
             <div class="flex flex-col">
-                <div class="pb-8 flex justify-between items-center">
+                <div class="pb-8 flex items-center">
                     <span class="font-bold text-xl">Orders</span>
-                    <el-button color="#626aef" style="font-size: medium" round size="large" @click="createAuction"
-                        >Create Auction</el-button
-                    >
                 </div>
                 <div v-if="listSellerHistorys.length" class="w-full flex flex-col items-center justify-center gap-4 relative pb-16">
                     <div
@@ -125,9 +140,7 @@ const getHistory = async (currentPage, pageSize, searchQuery, status) => {
     }
 }
 
-const filterStatus = async (value) => {
-    selectedItem.value = value
-    status.value = value
+const filterStatus = async () => {
     await SearchHistory()
 }
 
