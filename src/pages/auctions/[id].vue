cc2011910e0edd754d36f2773327ed9c58f20ece
Thanhnhan1409 <phanthithanhnhan1409@gmail.com>
2024-06-07 23:38:27 +0700

    fix: select category in header

--- src/pages/auctions/[id].vue
+++ src/pages/auctions/[id].vue
@@ -404,7 +404,7 @@ onBeforeMount(async () => {
                 <div class="cursor-pointer flex items-center gap-2 p-2 border rounded-lg w-full"
                     @click="goToSellerInfor(auction.user.id)">
                     <img class="w-[35px] h-[35px] object-contain rounded-full"
-                        :src="auction.user.avatar"
+                        :src="auction.user?.avatar"
                         alt="" />
                     <div class="flex flex-col">
                         <span>{{ auction.user && auction.user.fullName }}</span>
