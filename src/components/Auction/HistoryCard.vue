6ebc30f77954e04c127f19d751865065328f3c13
Thanhnhan1409 <phanthithanhnhan1409@gmail.com>
2024-06-09 21:55:56 +0700

    fix: create auction

--- src/components/Auction/HistoryCard.vue
+++ src/components/Auction/HistoryCard.vue
@@ -28,7 +28,7 @@
                         <div class="flex w-full items-center justify-between">
                             <div>
                                 <span class="mt-1 text-sm text-[#505050]">Price: </span>
-                                <span class="text-[#505050]">${{ auction.currentPrice }}</span>
+                                <span class="text-[#505050]">{{ formatNumber(auction.currentPrice) }} VNĐ</span>
                             </div>
                             <!-- <div>
                             <span class="text-[#505050]">Bid count: </span>
@@ -51,12 +51,12 @@
                     </div>
                     <div class="flex flex-col gap-2 items-end">
                         <div>
-                            <div class="flex items-center gap-2">
+                            <!-- <div class="flex items-center gap-2">
                                 <Icon :icon="icons[auction.productStatus]" />
                                 <span :style="{ color: getColor(auction.productStatus) }">{{
                                     ProductStatusNames[auction.productStatus]
                                 }}</span>
-                            </div>
+                            </div> -->
                             <div class="flex items-center gap-2">
                                 <span class="inline-block text-sm text-[#505050]">Bid count: </span>
                                 <span class="inline-block text-sm text-[#505050]">{{ auction.bidCount }}</span>
@@ -97,6 +97,7 @@ import { useRouter } from 'vue-router'
 import { productStatus } from '../../utils/Enums/ProductStatus.js'
 import { cancelAuction} from '../../services/auction.service'
 import { useAuctionStore } from '../../stores/auction.store.js';
+import { formatNumber } from '../../utils'
 
 
 defineProps({
@@ -110,19 +111,6 @@ defineProps({
 const useAuction = useAuctionStore()
 const router = useRouter()
 const role = localStorage.getItem('role')
-
-
-// const url = auction.mediaUrls[0] || 'https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg'
-// const srcList = auction.mediaUrls || [
-//     'https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg',
-//     'https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg',
-//     'https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg',
-//     'https://fuss10.elemecdn.com/0/6f/e35ff375812e6b0020b6b4e8f9583jpeg.jpeg',
-//     'https://fuss10.elemecdn.com/9/bb/e27858e973f5d7d3904835f46abbdjpeg.jpeg',
-//     'https://fuss10.elemecdn.com/d/e6/c4d93a3805b3ce3f323f7974e6f78jpeg.jpeg',
-//     'https://fuss10.elemecdn.com/3/28/bbf893f792f03a54408b3b7a7ebf0jpeg.jpeg',
-//     'https://fuss10.elemecdn.com/2/11/6535bcfb26e4c79b48ddde44f4b6fjpeg.jpeg',
-// ]
 
 const ProductStatusNames = productStatus
 
